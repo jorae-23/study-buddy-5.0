66010c737be0d6c419f4c792fa648084382b0d94
jorae-24 <alvinjorkey@influencewm.com>
2023-02-28 22:25:10 -0600

    table reservation

--- App.tsx
+++ App.tsx
@@ -1,34 +1,69 @@
 import React from 'react';
 import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
 import NfcManager, {NfcTech} from 'react-native-nfc-manager';
+import { Alert } from 'react-native';
+import axios from 'axios';
+
 
 // Pre-step, call this before any NFC operations
 NfcManager.start();
 
 function App() {
-  async function readNdef() {
+
+  async function getTagNumber(tagId : String | undefined){
+    let tagNum
     try {
+        const response = await axios.get(`http://10.20.45.60:3001/data/api/${tagId}`)
+        tagNum = response.data[0].TagNum
+    } catch(error) {
+        console.warn('something went wrong', error)
+    }
+    return tagNum
+  }
+
+  async function getTableNum(tagNum : number | undefined){
+    let tableNum
+    try {
+        const response = await axios.get(`http://10.20.45.60:3001/data/api/g/${tagNum}`)
+        tableNum = response.data[0].TableNum
+    } catch(error) {
+        console.warn('something went wrong', error)
+    }
+    return tableNum
+  }
+
+  async function reserveTable() {
+    try {
+      Alert.alert('Scan button pressed', 'looking for tag to scan')
+      
       // register for the NFC tag with NDEF in it
       await NfcManager.requestTechnology(NfcTech.Ndef);
+
       // the resolved tag object will contain `ndefMessage` property
       const tag = await NfcManager.getTag();
-      console.warn('Tag found', tag);
+
+      const tagID = tag?.id
+      const tagNum =  await getTagNumber(tagID)
+      const tableNum = await getTableNum(tagNum)
+      Alert.alert(`You have reserved table ${tableNum} from scanning tag ${tagNum}`)
+
     } catch (ex) {
       console.warn('Oops!', ex);
     } finally {
+
       // stop the nfc scanning
       NfcManager.cancelTechnologyRequest();
     }
-  }
+  } 
 
   return (
     <View style={styles.wrapper}>
-      <TouchableOpacity onPress={readNdef}>
-        <Text>Scan a Tag</Text>
+      <TouchableOpacity onPress={reserveTable}>
+        <Text>Scan a Tag!</Text>
       </TouchableOpacity>
     </View>
   );
-}
+} 
 
 const styles = StyleSheet.create({
   wrapper: {
