2488688f70df7017cd2d6261f5d2d9f0cb526641
wilbt-22 <benwilson00@gmail.com>
2023-04-23 16:06:28 -0500

    array of tables to check database

--- screens/LibFloorPlan.tsx
+++ screens/LibFloorPlan.tsx
@@ -1,5 +1,5 @@
 //import React, {useState}from 'react';
-import React from 'react';
+import React, { useState } from 'react';
 import {View, Text, TouchableOpacity, StyleSheet, SafeAreaView, ImageBackground, Image, Dimensions} from 'react-native';
 import NfcManager, {NfcTech} from 'react-native-nfc-manager';
 import { Alert } from 'react-native';
@@ -25,51 +25,85 @@ const screenHieght = height;
 
 /* Here i'm thinking a getTableStatus function that takes in a table number, checks the database, if open then return the string "green", else return "red" */
  
-const getTableStatus = (tableNumber: number | undefined) => {
-    
-    const status = 'occu' ;//(check database);
-    const open = 'open';
 
-   
-    if (open.localeCompare(status) == 0){
-        const color = 'green';
-        return color;
-    }
-    const color = 'red';
-    return color;
-
+async function getNumOfStudents(tableNum: number | undefined){
+    const response = await axios.get(`http://44.203.31.97:3001/data/api/g/totalStudents/${tableNum}`)
+    const numOStu:number =  await response.data[0].num_occupied_seats
+    //setStudentsAtTable(numOStu)
+    return numOStu;
 };
 
 
-const table2color = "green";
-const table3color = "yellow";
+
 
 export default function LibFloorPlan(){
+    //array of 10 tables initialized to empty
+    const statusArray = Array(10).fill(0);
+    
+    //get the number of students at each table
+    for( let i = 0; i < statusArray.length; i++){
+       statusArray[i] = getNumOfStudents(i);
+    }
+
+    function getTableStatus(tableNumber: number) {
+    
+        //await getNumOfStudents(tableNumber) ;//(check database);
+        
+        //status is number of students at a table
+        const status:number = statusArray[tableNumber];
+        
+        //if (tableNumber > 3){
+            //status = 'open';
+        //}
+    
+        // no students
+        if (status == 0){
+            const color = 'green';
+            return color;
+        }
+        //table has students
+        if (status > 0 ){
+            const color = 'red';
+            return color;
+        }
+        // sanity check if the if statements arent checked properly
+        const color = "blue";
+        return color;
+    };
    
+
     return(
         
         <SafeAreaView style = {styles.SafeAreaViewContainer}>
 
+            
             <Image 
                 style= {styles.floor}
                 source={require('./FirstLevelCropped.png')} 
             />
             <View style = {styles.tables}>
-                
-                
+
                 <Canvas style={styles.tables}>
                     {/* large tables */}
                     
                     <Box color = {getTableStatus(1)} box= {rrect(rect(screenWidth/2 +90, screenHieght/2 +135, 20, 20), 5, 5)}></Box>
                     <Box color = {getTableStatus(2)} box= {rrect(rect(screenWidth/2 +90, screenHieght/2 +105, 20, 20), 5, 5)}></Box>
-                    <Box color={getTableStatus(3)} box= {rrect(rect(screenWidth/2 +60, screenHieght/2 +135, 20, 20), 5, 5)}></Box>
-                    <Box color = "green" box= {rrect(rect(screenWidth/2 +60, screenHieght/2 +105, 20, 20), 5, 5)}></Box>
-                    <Box color = "green" box= {rrect(rect(screenWidth/2 +50, screenHieght/2 +62, 20, 20), 5, 5)}></Box>
+                    <Box color= {getTableStatus(3)} box= {rrect(rect(screenWidth/2 +60, screenHieght/2 +135, 20, 20), 5, 5)}></Box>
+                    <Box color = { getTableStatus(4)} box= {rrect(rect(screenWidth/2 +60, screenHieght/2 +105, 20, 20), 5, 5)}></Box>
+                    <Box color = { getTableStatus(5)} box= {rrect(rect(screenWidth/2 +50, screenHieght/2 +62, 20, 20), 5, 5)}></Box>
+                    
                     {/* single seats */}
-                    <Box color = "red" box= {rrect(rect(screenWidth/2 +96, screenHieght/2 +79, 10, 10), 2, 2)}></Box>
-                    <Box color = "green" box= {rrect(rect(screenWidth/2 +96, screenHieght/2 +70, 10, 10), 2, 2)}></Box>
+                
+                    <Box color = { getTableStatus(6)} box= {rrect(rect(screenWidth/2 +96, screenHieght/2 +79, 10, 10), 2, 2)}></Box>
+                    
+                    {/* table 8 in the database is currently occupied, should show up red */}
+                    <Box color = { getTableStatus(8)} box= {rrect(rect(screenWidth/2 +96, screenHieght/2 +70, 10, 10), 2, 2)}></Box>
+
+                    
                 </Canvas>
+                
             </View>
+            
         </SafeAreaView>
     )
 }
